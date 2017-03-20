616cb466390a08c4c12bbe9c8fd3e267d502724c
plohej <plohej@example.com>
2017-03-20 18:59:52 +0100

    prawie dziala

--- zad3/src/test/java/pl/artur/pjwst/zad3/AlarmClockTest.java
+++ zad3/src/test/java/pl/artur/pjwst/zad3/AlarmClockTest.java
@@ -10,9 +10,12 @@ import org.easymock.TestSubject;
 import org.joda.time.DateTime;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import junit.framework.TestCase;
 
+@RunWith ( JUnit4. class )
 public class AlarmClockTest extends TestCase {
 
 	@Rule
@@ -22,21 +25,21 @@ public class AlarmClockTest extends TestCase {
     public AlarmClock alarmClock = new AlarmClockImpl();
 
     @Mock
-    public DateTime time;
+    public DateTime now = new DateTime();
 	
 	@Test
 	public void testAlarmShouldRing() {
 		DateTime timeRing = new DateTime(1995,1,1,12,30);
         alarmClock.addAlarmTime(timeRing);
-        expect(alarmClock.shouldRing())
-                .andReturn(true)
-                .andReturn(false).times(2)
-                .andReturn(true);
-        replay(alarmClock);
+        expect(now.withTimeAtStartOfDay())
+                .andReturn(new DateTime(1995,1,1,12,30))
+                .andReturn(new DateTime(1995,1,1,12,30)).times(2)
+                .andReturn(new DateTime(1995,1,1,12,30));
+        replay(now);
         assertEquals(true, alarmClock.shouldRing());
         assertEquals(false, alarmClock.shouldRing());
         assertEquals(false, alarmClock.shouldRing());
         assertEquals(true, alarmClock.shouldRing());
-        verify(alarmClock);
+        verify(now);
 	}
 }
