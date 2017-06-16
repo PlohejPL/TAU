03b1eb2c0f6bd9dc52f388bc4988136101964516
PlohejPL <s13384@pjwstk.edu.pl>
2017-06-17 00:23:56 +0200

    dodanie testow jednostkowych

--- zad12-cpptesty/src/tests/units.cpp
+++ zad12-cpptesty/src/tests/units.cpp
@@ -1,16 +1,20 @@
 #include <sortlib.hpp>
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include <iostream>
 
-void Test() {
+TEST_CASE("Tab sorts sorted array") {
     QuickSortR qs;
     const int size = 4;
     int tab[size];
-    for(int i=0; i<size; i++)
+    for(int i=0; i<size; i++){
+	tab[i]=1;
+    }
     REQUIRE(&tab != NULL);
     REQUIRE_NOTHROW(qs.do_sort(tab, size));
-    REQUIRE(tab[0] < tab[1]);
-    REQUIRE(tab[1] < tab[2]);
-	REQUIRE(tab[2] < tab[3]);
-	REQUIRE(2 == 1);
+    REQUIRE(tab[0] <= tab[1]);
+    REQUIRE(tab[1] <= tab[2]);
+	REQUIRE(tab[2] <= tab[3]);
+	//REQUIRE(2 == 1);
+	std::cout << "==+++++++++++++++++++++++++++++++++++++++++++++++++++==" << std::endl;
 }
