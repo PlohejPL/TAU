63b5a6b5bbd0ad51b0686785bc9c95435484c528
PlohejPL <s13384@pjwstk.edu.pl>
2017-06-20 09:57:32 +0200

    zmiana polozenia testu jednostkowego

--- zad12-cpptesty/src/tests/unit_sortlib.cpp
+++ zad12-cpptesty/src/tests/unit_sortlib.cpp
@@ -2,6 +2,23 @@
 
 #include <catch.hpp>
 
+
+
+TEST_CASE("Tab sorts sorted array") {
+    QuickSortR qs;
+    const int size = 4;
+    int tab[size];
+    for(int i=0; i<size; i++){
+	tab[i]=1;
+    }
+    REQUIRE(&tab != NULL);
+    REQUIRE_NOTHROW(qs.do_sort(tab, size));
+    REQUIRE(tab[0] <= tab[1]);
+    REQUIRE(tab[1] <= tab[2]);
+    REQUIRE(tab[2] <= tab[3]);
+    //REQUIRE(2 == 1);//asda
+}
+
 SCENARIO("Sorting array with Quick Sort") {
     QuickSortR qs;
     const int size = 4;
